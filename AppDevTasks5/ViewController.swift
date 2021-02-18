60c65cb87065818474f1ac359465ee986c4b8aad
Yanmo <na2key@icloud.com>
2021-02-18 19:35:32 +0900

    Initial Commit

---
+++ AppDevTasks5/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  AppDevTasks5
+//
+//  Created by 山本 夏紀 on 2021/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet private weak var leftTextField: UITextField!
+    @IBOutlet private weak var rightTextField: UITextField!
+    @IBOutlet private weak var button: UIButton!
+    @IBOutlet private weak var resultLabel: UILabel!
+    private func presentAlert(alertMassage: String) {
+        let alert = UIAlertController(title: "WORNING", message: alertMassage, preferredStyle: .alert)
+        present(alert, animated: true, completion: nil)
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okButton)
+    }
+    @IBAction func buttonPressed(_ sender: Any) {
+        guard let leftTexitFieldNum = Double(leftTextField.text!) else {
+            presentAlert(alertMassage: "割られる数を入力して下さい")
+            return
+        }
+        guard let rightTexitFieldNum = Double(rightTextField.text!) else {
+            presentAlert(alertMassage: "割る数を入力して下さい")
+            return
+        }
+        guard rightTexitFieldNum != 0 else {
+            presentAlert(alertMassage: "0では割れません")
+            return
+        }
+        resultLabel.text = String(leftTexitFieldNum / rightTexitFieldNum)
+    }
+}
---
+++ AppDevTasks5Tests/AppDevTasks5Tests.swift
@@ -0,0 +1,33 @@
+//
+//  AppDevTasks5Tests.swift
+//  AppDevTasks5Tests
+//
+//  Created by 山本 夏紀 on 2021/02/18.
+//
+
+import XCTest
+@testable import AppDevTasks5
+
+class AppDevTasks5Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
